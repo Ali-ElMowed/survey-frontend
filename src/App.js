879bf7685c2f5ffdcf204b6140fda36bc9eaa12b
Your NameAliAlMowed <you@example.comalmowed.ali@gmail.com>
2022-06-20 00:21:08 +0300

    wrapped app with redux

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Header from './components/Header';
 import Add_question_card from './components/Add_question_card';
-import Choices from './components/Choices';
-
+import store from './redux/store'
+import { Provider, useSelector } from 'react-redux'
 function App() {
+  const survey = useSelector(state => state.survey)
   return (
-    <Router>
-        <Header title={"Survey Builder"}/>
-        <Add_question_card/>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Header title={"Survey Builder"} />
+        {
+          survey?.questions?.map(question => (<Add_question_card data={question} />))
+        }
+
+      </Router>
+    </Provider>
   );
 }
 
